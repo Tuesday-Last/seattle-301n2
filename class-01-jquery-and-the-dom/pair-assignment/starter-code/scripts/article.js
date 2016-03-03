23a381022774a8621ee18ee379d7c4908c6c8281
Tuesday-Last <sgnortz@gmail>
2016-03-02 19:40:37 -0800

    halfway push

--- class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
@@ -1,23 +1,37 @@
 var articles = [];
 
-function Article (opts) {
-  // TODO: Use the js object passed in to complete this contructor function:
-  // Save ALL the properties of `opts` into `this`.
-  this.author = opts.author;
+function Article (objt) {
+  this.title = objt.title;
+  this.category = objt.category
+  this.author = objt.author;
+  this.authorURL = objt.authorURL;
+  this.publishedOn = objt.publishedOn;
+  this.body = objt.body;
+  articles.push(this);
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
 
   $newArticle.attr('data-category', this.category);
-
+  $newArticle.attr('author', this.author);
+  $newArticle.attr("authorURL", this.authorURL);
+  $newArticle.attr("title", this.title);
+  $newArticle.attr("body", this.body);
+  $newArticle.attr("publishedOn", this.publishedOn);
   // TODO: Use jQuery to fill in the template with properties
   // from this particular Article instance. We need to fill in:
   // the author name and url, the article title and body, and the
   // publication date.
 
   // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
+
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn).hide();
+  $("title").hover(
+    function(){
+      "publishedOn".show();
+    };
+  );
 
   // Display the date as a relative number of "days ago":
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
