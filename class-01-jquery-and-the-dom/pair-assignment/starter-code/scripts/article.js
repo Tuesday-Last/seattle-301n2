f07e9e212e3135f20770bbc849288a91664c51f8
Tuesday-Last <sgnortz@gmail>
2016-03-03 14:34:41 -0800

    got most of it working...

--- class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
@@ -2,13 +2,12 @@ var articles = [];
 
 function Article (objt) {
   this.title = objt.title;
-  this.category = objt.category
+  this.category = objt.category;
   this.author = objt.author;
   this.authorURL = objt.authorURL;
   this.publishedOn = objt.publishedOn;
   this.body = objt.body;
-  articles.push(this);
-}
+};
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
@@ -19,19 +18,22 @@ Article.prototype.toHtml = function() {
   $newArticle.attr("title", this.title);
   $newArticle.attr("body", this.body);
   $newArticle.attr("publishedOn", this.publishedOn);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('.article-body').html(this.body)
+  $newArticle.find("div.byline > address").html("<a href=" + this.authorURL + ">" + this.author + "</a>");
+  $newArticle.find("div.byline > time").html(this.publishedOn);
+
   // TODO: Use jQuery to fill in the template with properties
   // from this particular Article instance. We need to fill in:
   // the author name and url, the article title and body, and the
   // publication date.
 
   // Include the publication date as a 'title' attribute to show on hover:
-
+  
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn).hide();
-  $("title").hover(
-    function(){
-      "publishedOn".show();
-    };
-  );
+  // $newArticle.attr("title").hover(function(){
+  //     "publishedOn".show();
+  //   });
 
   // Display the date as a relative number of "days ago":
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
@@ -41,7 +43,7 @@ Article.prototype.toHtml = function() {
   // TODO: This cloned article is no longer a template, so we should remove that class...
 
   return $newArticle;
-}
+};
 
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
@@ -49,7 +51,7 @@ rawData.sort(function(a,b) {
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-})
+});
 
 articles.forEach(function(a){
   $('#articles').append(a.toHtml())
