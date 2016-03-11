8c23f1bb8808d25b66746aab3e103877f8ed4da5
Tuesday-Last <sgnortz@gmail>
2016-03-10 19:12:36 -0800

    refactored to avoid duplicates and simplified

--- class-04-templates/pair-assignment/Maddie&Sam/scripts/article.js
+++ class-04-templates/pair-assignment/Maddie&Sam/scripts/article.js
@@ -16,16 +16,17 @@ Article.prototype.toHtml = function() {
   //       - Now "compile" your template with Handlebars.
   var templateToCompile = Handlebars.compile(getTemplate);
 
-  var context = {
-    author: this.author,
-    authorUrl: this.authorUrl,
-    title: this.title,
-    category: this.category,
-    body: this.body,
-    publishedOn: this.publishedOn,
-  };
 
-  var compiledTemplate = templateToCompile(context);
+  // var context = {
+  //   author: this.author,
+  //   authorUrl: this.authorUrl,
+  //   title: this.title,
+  //   category: this.category,
+  //   body: this.body,
+  //   publishedOn: this.publishedOn,
+  // };
+
+  
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -34,7 +35,8 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-  $('#articlesGoHere').append(compiledTemplate);
+  // $('#articlesGoHere').append(compiledTemplate);
+  return templateToCompile(this);
 };
 
 rawData.sort(function(a,b) {
