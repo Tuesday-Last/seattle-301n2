676928f71a2e8e872fe63092d9107e392a78f5e2
Tuesday-Last <sgnortz@gmail>
2016-03-13 15:10:05 -0700

    corrected problems with author filter

--- class-04-templates/pair-assignment/Maddie&Sam/scripts/article.js
+++ class-04-templates/pair-assignment/Maddie&Sam/scripts/article.js
@@ -26,7 +26,7 @@ Article.prototype.toHtml = function() {
   //   publishedOn: this.publishedOn,
   // };
 
-  
+
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
--- class-04-templates/pair-assignment/Maddie&Sam/scripts/articleView.js
+++ class-04-templates/pair-assignment/Maddie&Sam/scripts/articleView.js
@@ -3,16 +3,14 @@ var articleView = {};
 
 articleView.populateFilters = function() {
   $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var val = $(this).find('address a').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      $('#author-filter').append(optionTag);
+    var val = $(this).find('address a').text();
+    var optionTag = '<option value="' + val + '">' + val + '</option>';
+    $('#author-filter').append(optionTag);
 
-      val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
-      }
+    val = $(this).attr('data-category');
+    optionTag = '<option value="' + val + '">' + val + '</option>';
+    if ($('#category-filter option[value="' + val + '"]').length === 0) {
+      $('#category-filter').append(optionTag);
     }
   });
 };
